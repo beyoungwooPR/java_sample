91e70adb78ad04509a2dc87b1f0178a440dd223d
Beyoungwoo Server <beyoungwoo@y1j.sfla.fortatrust.com>
2016-08-18 22:02:32 +0900

    Add array code

--- src/com/y1/java_se6/array/Array.java
+++ src/com/y1/java_se6/array/Array.java
@@ -2,4 +2,28 @@ package com.y1.java_se6.array;
 
 public class Array {
 
+    public static void array1 () {
+        int[] arr1 = {1, 2, 3, 4, 5};
+        int[] tmp1 = arr1;
+        int[] tmp2 = arr1;
+
+        System.out.println("=============[1]=============");
+        System.out.print("tmp1：");
+        for(int i = 0; i < tmp1.length; i++)
+            System.out.print(tmp1[i] + " ");
+
+        System.out.print("\ntmp2：");
+        for(int i = 0; i < tmp2.length; i++)
+            System.out.print(tmp2[i] + " ");
+
+        tmp1[2] = 9;
+        System.out.print("\ntmp1：");
+        for(int i = 0; i < tmp1.length; i++)
+            System.out.print(tmp1[i] + " ");
+
+        System.out.print("\ntmp2：");
+        for(int i = 0; i < tmp2.length; i++)
+            System.out.print(tmp2[i] + " ");
+        System.out.println();
+    }
 }
